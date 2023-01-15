fba3c9f474eb7cde2da08ba052dfa16ca902f011
jas4code <jasabocejo@protonmail.com>
2023-01-16 00:34:19 +0800

    Kanban Board
    Practiced reading documentation and using some basic jQuery functions

---
+++ app.js
@@ -0,0 +1,10 @@
+// <!-- NOTE: I can't use the external links for changing the background UI -->
+    // <script>
+        // $( function() {
+        //     $( "div ul li" ).draggable();
+        // } );
+
+        $( function() {
+            $( ".tasks" ).sortable();
+        } );
+    // </script>

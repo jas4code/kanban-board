3dce6a6a9cf8a7bd3fd5172f59173d918ee8fe06
jas4code <jasabocejo@protonmail.com>
2023-01-16 00:41:45 +0800

    Update the file name/system

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-// <!-- NOTE: I can't use the external links for changing the background UI -->
-    // <script>
-        // $( function() {
-        //     $( "div ul li" ).draggable();
-        // } );
-
-        $( function() {
-            $( ".tasks" ).sortable();
-        } );
-    // </script>
+$(document).ready(function(){
+    $( function() {
+        $( ".tasks" ).sortable();
+    });
+});
